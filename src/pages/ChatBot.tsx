ae898acf074e39eb71f89ae8dea6f006003b2d90
Abel Ilito Limoma <ilitoabel@gmail.com>
2025-10-24 02:51:41 +0200

    Feat: add style

--- src/pages/ChatBot.tsx
+++ src/pages/ChatBot.tsx
@@ -1,7 +1,6 @@
 import { useChatStore } from "@/store/useChatStore";
 import ChatForm from "../components/ChatFrom";
 import ChatMessages from "../components/ChatMessages";
-import ClearChat from "@/components/ClearChat";
 
 export const ChatBot = () => {
   const messages = useChatStore((state) => state.messages);
@@ -15,11 +14,15 @@ export const ChatBot = () => {
   return (
     <>
       <div className="flex-1 items-center flex flex-col gap-10">
-        <div className="flex justify-end w-[50%]">
-          <ClearChat />
-        </div>
         {handleShowMessages()}
-        <ChatForm />
+        <div
+          className={
+            "bg-white flex justify-center pb-[5rem] w-full " +
+            (messages.length > 0 && "fixed bottom-0")
+          }
+        >
+          <ChatForm />
+        </div>
       </div>
     </>
   );
